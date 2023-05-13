7c25e894ef40b8a81a57e038b3d14ec01babdd06
MaDnNeSsKoOpi <96216773+MaDnNeSsKoOpi@users.noreply.github.com>
2023-05-13 18:14:42 +0300

    second commit 4.222

--- src/pages/api/load-cart.js
+++ src/pages/api/load-cart.js
@@ -20,8 +20,8 @@ export default async function handler(_req, res) {
 
     const data = await postToShopify({
         query: `
-        query GetCart($realCartId: ID!) {
-          cart(id: $realCartId) {
+        query GetCart($cartId: ID!) {
+          cart(id: $cartId) {
             checkoutUrl
             estimatedCost {
               totalAmount {
